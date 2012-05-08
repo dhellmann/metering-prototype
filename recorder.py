4c32195bd731dcd1251c8bca37422a9251cc2c0f
Doug Hellmann <doug.hellmann@dreamhost.com>
2012-05-08 15:24:00 -0400

    program to record events for playback; intended to be used for testing

---
+++ recorder.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import cPickle as pickle
+import logging
+
+import notificationclient
+
+
+LOG = logging.getLogger(__name__)
+
+
+import argparse
+import sys
+
+from kombu import BrokerConnection
+
+parser = argparse.ArgumentParser(
+    description='record notification events for playback',
+    )
+parser.add_argument('output_file',
+                    help='the data file to create',
+                    )
+args = parser.parse_args()
+
+console = logging.StreamHandler(sys.stderr)
+console.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(message)s')
+console.setFormatter(formatter)
+root_logger = logging.getLogger('')
+root_logger.addHandler(console)
+root_logger.setLevel(logging.DEBUG)
+
+with open(args.output_file, 'wb') as output:
+
+    def process_event(body, message):
+        pickle.dump(body, output)
+        message.ack()
+
+    with BrokerConnection('amqp://guest:secrete@localhost//') as conn:
+        handler = notificationclient.NotificationClient(conn, process_event)
+        try:
+            handler.run()
+        except KeyboardInterrupt:
+            pass
+        print 'Exiting'
