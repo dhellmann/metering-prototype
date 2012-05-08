7853829ea63e2887697f555e4218499909d11df2
Doug Hellmann <doug.hellmann@dreamhost.com>
2012-05-08 11:23:20 -0400

    cleanup and add hook methods

--- raw_listener.py
+++ raw_listener.py
@@ -6,26 +6,34 @@ from pprint import pprint
 from kombu import Exchange, Queue
 from kombu.mixins import ConsumerMixin
 
-nova_exchange = Exchange(name='nova',
-                         type='topic',
-                         durable=False,
-                         auto_delete=False,
-                         )
-instance_exists = Queue(name='notifications.info',
-                        exchange=nova_exchange,
-                        routing_key='notifications.info',
-                        durable=False,
-                        auto_delete=False,
-                        )
-
 
 class MessageHandler(ConsumerMixin):
+
+    queue = Queue(name='notifications.info',
+                  exchange=Exchange(name='nova',
+                                    type='topic',
+                                    durable=False,
+                                    auto_delete=False,
+                                    ),
+                  routing_key='notifications.info',
+                  durable=False,
+                  auto_delete=False,
+                  )
 
     def __init__(self, connection):
         self.connection = connection
 
+    def on_consume_ready(self, *args, **kwds):
+        print 'Ready to receive', args, kwds
+
+    def on_consume_end(self, *args, **kwds):
+        print 'Done', args, kwds
+
+    def on_iteration(self, *args, **kwds):
+        pass
+
     def get_consumers(self, Consumer, channel):
-        return [Consumer(queues=[instance_exists],
+        return [Consumer(queues=[self.queue],
                          callbacks=[self.process_event],
                          )
                 ]
